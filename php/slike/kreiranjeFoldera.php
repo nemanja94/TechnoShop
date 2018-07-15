e24339d24dc9d15d356bbfb4662803f6ae3828f4
Nemanja Andric <aandric.nemanja@gmail.com>
2018-07-15 11:41:40 +0200

    pocinje

--- php/slike/kreiranjeFoldera.php
+++ php/slike/kreiranjeFoldera.php
@@ -1,17 +1,23 @@
 <?php
 
-echo 'Pocinje';
+echo 'Pocinje - 3\n';
 
 require '../baza/Database.php';
 
+echo 'PDO - 7\n';
 $pdo = Database::connect();
 
+echo 'Upit - 10\n';
 $query = $pdo->prepare('SELECT *  FROM `Artikl` ORDER BY `Artikl`.`artikl_sifra` ASC;');
 
+echo 'Izvrsenje upita - 13\n';
 $query->execute();
 
+echo 'Raskidanje konekcije - 16\n';
 Database::disconnect();
 //Dodati kod za kreiranje direktorijuma
+
+echo 'While petlja - 20\n';
 while ($sifra = $query->fetch()) {
     $path = "/var/www/html/projects/TechnoShop/php/slike/" . $sifra;
     if (mkdir($path, 0777)) {
