3f578ef1f36d88e93ba494b543a2604d4410a70e
Nemanja Andric <aandric.nemanja@gmail.com>
2018-07-15 11:51:49 +0200

    pocinje

--- php/slike/kreiranjeFoldera.php
+++ php/slike/kreiranjeFoldera.php
@@ -4,27 +4,31 @@ echo "Pocinje - 3\n";
 
 require "../baza/Database.php";
 
-echo "PDO - 7\n";
-$pdo = Database::connect();
+try {
+    echo "PDO - 7\n";
+    $pdo = Database::connect();
 
-echo "Upit - 10\n";
-$query = $pdo->prepare("SELECT artikl_sifra FROM `Artikl` ORDER BY `Artikl`.`artikl_sifra` ASC");
+    echo "Upit - 10\n";
+    $query = $pdo->prepare("SELECT artikl_sifra FROM `Artikl` ORDER BY `Artikl`.`artikl_sifra` ASC");
 
-echo "Izvrsenje upita - 13\n";
-$query->execute();
+    echo "Izvrsenje upita - 13\n";
+    $query->execute();
 
-echo "Raskidanje konekcije - 16\n";
-Database::disconnect();
-//Dodati kod za kreiranje direktorijuma
+    echo "Raskidanje konekcije - 16\n";
+    Database::disconnect();
+    //Dodati kod za kreiranje direktorijuma
 
-echo "While petlja - 20\n";
-while ($sifra = $query->fetch()) {
-    $path = "/var/www/html/projects/TechnoShop/php/slike/" . $sifra;
-    if (mkdir($path, 0777, true)) {
-        chmod($path, 0777);
-        echo "Folder " . $sifra . " je kreiran.";
-    } else {
-        echo "Folder " . $sifra . " nije kreiran.";
+    echo "While petlja - 20\n";
+    while ($sifra = $query->fetch()) {
+        $path = "/var/www/html/projects/TechnoShop/php/slike/" . $sifra;
+        if (mkdir($path, 0777, true)) {
+            chmod($path, 0777);
+            echo "Folder " . $sifra . " je kreiran.";
+        } else {
+            echo "Folder " . $sifra . " nije kreiran.";
+        }
     }
+    //header("Location: unosArtikl.php");
+} catch (PDOException $e) {
+    echo $e->getMessage();
 }
-//header("Location: unosArtikl.php");
