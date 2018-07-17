93f9b51ad6a5c8f15e3cc7ee9988cc1379e445ce
Nemanja Andric <aandric.nemanja@gmail.com>
2018-07-17 16:10:57 +0200

    Pocinje

--- php/slike/unosSlika.php
+++ php/slike/unosSlika.php
@@ -106,7 +106,7 @@ include_once('../header.php');
             $pdo = Database::connect();
 
             $queryArtikli = $pdo->prepare(
-                'SELECT * FROM TechnoShop.Artikl'
+                'SELECT * FROM TechnoShop.Artikl limit 10'
             );
 
             $queryArtikli->execute();
