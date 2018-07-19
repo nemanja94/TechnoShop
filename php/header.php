6d2f6ef471eb8bccb74c28a6f9205f5d6f96d69e
Nemanja Andric <aandric.nemanja@gmail.com>
2018-07-19 19:03:04 +0200

    Nesto

--- php/header.php
+++ php/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Techno Shop<?php $strana ?></title>
+    <title>Techno Shop<?php echo $strana ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="../../css/stil.css"> -->
 </head>
